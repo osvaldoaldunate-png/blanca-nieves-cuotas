4d12f95d402bf2770428718cb5ea1dd429effa43
osvaldoaldunate-png <osvaldo.aldunate@gmail.com>
2026-08-26 23:23:30 -0400

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,2 +1,2 @@
 window.BN_SUPABASE_URL = "https://eexixxriwuvafikjyrmm.supabase.co";
-window.BN_SUPABASE_ANON_KEY = "sb_publishable_CvEv4J1f0p4HJVbD7a2kEg_yMb7EjWI";
+window.BN_SUPABASE_ANON_KEY = "sb_publishable_CyEv4J1fOp4HJVbD7a2kEg_yMb7EjWI";
